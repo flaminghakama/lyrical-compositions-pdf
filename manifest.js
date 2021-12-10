485fbc5b82239b0d79ab9da5ee373c03eecedb22
David Elaine Alt <elaine@flaminghakama.com>
2021-12-09 18:22:48 -0800

    adding mm

--- manifest.js
+++ manifest.js
@@ -8,33 +8,63 @@ lyricalCompositionsSongs = {
 
     partsInBooks: {
         "Concert Lead Sheets": {
+            "Catch Me": [ "Concert Lead Sheet" ], 
             "Force of Nature": [ "Concert Lead Sheet" ], 
             "Frivolity": [ "Concert Lead Sheet" ], 
             "Jasmine": [ "Concert Lead Sheet" ], 
             "Just a Cup of Coffee": [ "Concert Lead Sheet" ], 
             "Mark Time": [ "Concert Lead Sheet" ], 
+            "News from Milan": [ "Concert Lead Sheet" ], 
             "Seven O'Clock Tune": [ "Concert Lead Sheet" ], 
             "Three Out of Four Nights": [ "Concert Lead Sheet" ], 
         },
     },
 
-// News from Milan
-// https://alttal.com/media/mp3/open-the-gates/News%20from%20Milan.mp3
-// https://alttal.com/media/pdf/open-the-gates/news-from-milan.pdf
-
 // Catch Me
 // https://alttal.com/media/mp3/open-the-gates/Catch%20Me.mp3
 // https://alttal.com/media/pdf/open-the-gates/catch-me.pdf
-
-// l'Amoureuse de mon Pere
-// https://alttal.com/media/mp3/open-the-gates/lAmoureuse%20de%20mon%20Pere.mp3
-// https://alttal.com/media/pdf/open-the-gates/lamoureuse-de-mon-pere.pdf
 
 // Elaine
 // https://alttal.com/media/mp3/open-the-gates/Elaine.mp3
 // https://alttal.com/media/pdf/open-the-gates/elaine.pdf
 
     songs: {
+
+        "Catch Me": {
+            metadata: { 
+                title: "Catch Me",
+                composer: "Elaine Paul",
+                genre: "Modern Jazz",
+                bpm: '168',
+                key: "Bb minor", 
+                time: "mixed meter",
+                date: "22 January 1999"
+            },
+            baseUrl: 'https://altjazz.org/scores/lyrical-compositions/', 
+            pdfSubdir: "",
+            fileLocation: "force-of-nature",
+            filePrefix: "force-of-nature-",
+            parts: {
+                "Concert Lead Sheet": { fileSuffix: "Lead-Sheet-C" },
+            },
+            scores: {
+                //"Concert Lead Sheet": { fileSuffix: "Lead-Sheet-C" }
+            },
+            recordings: {
+                album: {
+                    description: 'Track 3 from Open the Gates! as mp3',
+                    url: 'https://alttal.com/media/mp3/open-the-gates/Force%20of%20Nature.mp3'
+                },
+                youtube: {
+                    description: 'From Open the Gates! playlist on youtube',
+                    url: 'https://www.youtube.com/watch?v=NoYvp8pKftY&list=OLAK5uy_mRX13K7hUUUolZs7rAmpikIUX1j2czEUM&index=3'
+                },
+                streamed: {
+                    description: 'Twitch stream from Nonbinary Fully-Automated Luxury Gay Communism #3',
+                    url: 'https://www.twitch.tv/videos/1227467027'
+                },
+            }
+        },
 
         "Force of Nature": {
             metadata: { 
@@ -80,7 +110,7 @@ lyricalCompositionsSongs = {
                 bpm: '64',
                 key: "E", 
                 time: "4/4 with 3/4",
-                date: "2000"
+                date: "17 July 2000"
             },
             baseUrl: 'https://altjazz.org/scores/lyrical-compositions/', 
             pdfSubdir: "",
@@ -116,7 +146,7 @@ lyricalCompositionsSongs = {
                 bpm: '64',
                 key: "E", 
                 time: "3/4",
-                date: "2000"
+                date: "18 May 2000"
             },
             baseUrl: 'https://altjazz.org/scores/lyrical-compositions/', 
             pdfSubdir: "",
@@ -180,6 +210,7 @@ lyricalCompositionsSongs = {
             }
         },  
 
+
         "Mark Time": {
             metadata: { 
                 title: "Mark Time",
@@ -188,7 +219,7 @@ lyricalCompositionsSongs = {
                 bpm: '184',
                 key: "E", 
                 time: "3/4 with 4/4",
-                date: "2000"
+                date: "15 July 2000"
             },
             baseUrl: 'https://altjazz.org/scores/lyrical-compositions/', 
             pdfSubdir: "",
@@ -212,6 +243,38 @@ lyricalCompositionsSongs = {
                 streamed: {
                     description: 'Twitch stream from Nonbinary Fully-Automated Luxury Gay Communism #3',
                     url: 'https://www.twitch.tv/videos/1227467027'
+                },
+            }
+        },        
+
+        "News from Milan": {
+            metadata: { 
+                title: "News from Milan",
+                composer: "Elaine Paul",
+                genre: "Modern Jazz",
+                bpm: '180',
+                key: "B minor", 
+                time: "mixed meter",
+                date: "23 May 1999"
+            },
+            baseUrl: 'https://altjazz.org/scores/lyrical-compositions/', 
+            pdfSubdir: "",
+            fileLocation: "news-from-milan",
+            filePrefix: "news-from-milan-",
+            parts: {
+                "Concert Lead Sheet": { fileSuffix: "Lead-Sheet-C" },
+            },
+            scores: {
+                //"Concert Lead Sheet": { fileSuffix: "Lead-Sheet-C" }
+            },
+            recordings: {
+                album: {
+                    description: 'Track 5 from Open the Gates! as mp3',
+                    url: 'https://alttal.com/media/mp3/open-the-gates/News%20from%20Milan.mp3'
+                },
+                youtube: {
+                    description: 'From Open the Gates! playlist on youtube',
+                    url: 'https://www.youtube.com/watch?v=NoYvp8pKftY&list=OLAK5uy_mRX13K7hUUUolZs7rAmpikIUX1j2czEUM&index=5'
                 },
             }
         },        
